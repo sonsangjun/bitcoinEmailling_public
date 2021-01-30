48d64674cf2ae19738c2d63192959ff796418ceb
san9014 <san9014@gmail.com>
2021-01-30 16:28:46 +0900

    노

--- app/sh/devBuild.sh
+++ app/sh/devBuild.sh
@@ -1,26 +1,26 @@
 # making Timezone symbolLink
 ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
-rm -rf /tmp/autobit
+rm -rf /tmp/bitcoinEmailling
 
 cd ~/devWorkspace/bitcoinEmailling/app/
-mkdir /tmp/autobit
+mkdir /tmp/bitcoinEmailling
 
-cp -r * /tmp/autobit
-cd /tmp/autobit
+cp -r * /tmp/bitcoinEmailling
+cd /tmp/bitcoinEmailling
 
 #운영전용 명령어 (only Realmode)
-#mv /tmp/autobit/realDockerFile/Dockerfile_real /tmp/autobit/Dockerfile
+#mv /tmp/bitcoinEmailling/realDockerFile/Dockerfile_real /tmp/bitcoinEmailling/Dockerfile
 
 timestamp=`date +%Y%m%d%H%M`
-listenport=18001:18001
+listenport=18011:18011
 
 # build and Timezon Linking
-docker build --tag auto/bitcoindev:$timestamp .
-docker create --name autobitdev -p $listenport \
+docker build --tag auto/errmail:$timestamp .
+docker create --name errmaildev -p $listenport \
  -v /log/nodejs/:/log/nodejs/ \
  -v /etc/localtime:/etc/localtime:ro \
 -e TZ=Asia/Seoul \
-auto/bitcoindev:$timestamp 
+auto/errmail:$timestamp 
 
 echo 'docker build complete.'
 
--- app/sh/realBuild.sh
+++ app/sh/realBuild.sh
@@ -1,24 +1,24 @@
 # making Timezone symbolLink
 ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
-rm -rf /tmp/autobit
+rm -rf /tmp/bitcoinEmailling
 
 cd ~/devWorkspace/bitcoinEmailling/app/
-mkdir /tmp/autobit
+mkdir /tmp/bitcoinEmailling
 
-cp -r * /tmp/autobit
-cd /tmp/autobit
-mv /tmp/autobit/realDockerFile/Dockerfile_real /tmp/autobit/Dockerfile
+cp -r * /tmp/bitcoinEmailling
+cd /tmp/bitcoinEmailling
+mv /tmp/bitcoinEmailling/realDockerFile/Dockerfile_real /tmp/bitcoinEmailling/Dockerfile
 
 timestamp=`date +%Y%m%d%H%M`
-listenport=8001:8001
+listenport=8011:8011
 
 # build and Timezon Linking
-docker build --tag auto/bitcoin:$timestamp .
-docker create --name autobit -p $listenport \
+docker build --tag auto/errmail:$timestamp .
+docker create --name errmail -p $listenport \
  -v /log/nodejs/:/log/nodejs/ \
  -v /etc/localtime:/etc/localtime:ro \
 -e TZ=Asia/Seoul \
-auto/bitcoin:$timestamp 
+auto/errmail:$timestamp 
 
 echo 'docker build complete.'
 
